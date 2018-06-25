907cc75e491b0eb7abf2ea39dc08b44321e14692
Adam Sitnik <adam.sitnik@gmail.com>
2018-06-25 15:32:11 +0200

    add repro case

---
+++ repro/Repro/Program.cs
@@ -0,0 +1,18 @@
+﻿using Benchmarks;
+using System;
+
+namespace Repro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tests = new BasicTests();
+
+            // fails with
+            // Unhandled Exception: System.IO.FileLoadException: Could not load file or assembly 'System.Buffers, Version=4.0.2.0, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51' or one of its dependencies. 
+            // The located assembly's manifest definition does not match the assembly reference. (Exception from HRESULT: 0x80131040)
+            tests.BasicPipelines().GetAwaiter().GetResult(); 
+        }
+    }
+}
